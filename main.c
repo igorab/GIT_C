438fe9f5cdfb780ce0e819389d3aa1017f6863a8
Igor <igor.balakirev@mail.ru>
2023-01-22 10:40:52 +0300

    c main 2

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+int *generate_array(size_t N)
+{
+    int A[N];
+
+    for (int i=0; i<N; i++)
+    {
+        A[i] = i;
+    }
+
+    return A;
+}
+
+
 int main() {
+
+    int *M = generate_array(5);
+
+    for (int i=0; i<5; i++)
+    {
+        printf("%d\n" , M[i]);
+    }
+
     printf("Hello, World!\n");
     return 0;
 }
